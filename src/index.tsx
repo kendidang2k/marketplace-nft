2c169ecbe09149f836092b61ed7e650f400b7d3b
kendidang2k <kendidang04082k@gmail.com>
2022-06-12 17:28:11 +0700

    deo start duoc

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD:src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-=======
 import App from "App";
 import Providers from "Providers";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
->>>>>>> 5b9595b629a621be221c4c39f2900c82a4326141:src/index.tsx
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
